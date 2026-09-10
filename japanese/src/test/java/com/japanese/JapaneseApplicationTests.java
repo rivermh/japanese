751bdc30cdd00fb4c7f6b6272000ce0a7093c048
rivermh <kmh000904@gmail.com>
2026-09-10 10:04:26 +0900

    first commit

---
+++ japanese/src/test/java/com/japanese/JapaneseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.japanese;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JapaneseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
